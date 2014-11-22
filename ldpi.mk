913a28acf5e9425d76115b2e725a7c6111c215c5
HandyMenny <handymenny@outlook.com>
2014-11-22 23:23:27 +0100

    ldpi-common: Update 240x320 theme for TWRP
    Replace the old theme with a new one
    
    Change-Id: If79ef82ce3415ba620d4843da676ed7d3f7d58e2
    Signed-off-by: HandyMenny <handymenny@outlook.com>

--- ldpi.mk
+++ ldpi.mk
@@ -18,4 +18,4 @@
 DEVICE_PACKAGE_OVERLAYS += device/ldpi-common/overlay
 DEVICE_PACKAGE_OVERLAYS += device/ldpi-common/blank-overlay
 PRODUCT_PACKAGE_OVERLAYS += device/ldpi-common/blank-overlay
-TW_CUSTOM_THEME := device/ldpi-common/twrp-themes/240x320/res
+TW_CUSTOM_THEME := device/ldpi-common/twrp-theme/240x320/res
